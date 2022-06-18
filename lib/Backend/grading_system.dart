b21c8feea2d0f1f389cfee62aae5fd5255fc4864
ichbinsese <sesemerk@gmail.com>
2022-06-18 18:59:49 +0200

    durchschnitt ausrechnen sollte gehen

--- lib/Backend/grading_system.dart
+++ lib/Backend/grading_system.dart
@@ -36,6 +36,39 @@ class GradingSystem{
     ).toList();
   }
 
+  static double getAverage(Subject subject){
+    List<BigGrade> bigGradesSubject = getBigGradesBySubject(subject);
+    List<SmallGrade> smallGradesSubject = getSmallGradesBySubject(subject);
+    if(isAdvancedLevel){
+      double bigGradeAverage = 0;
+      double smallGradeAverage = 0;
+      double a = 0;
+      if(bigGrades.isNotEmpty){
+        bigGradesSubject.map((e) => bigGradeAverage + e.value);
+        bigGradeAverage /= bigGradesSubject.length as double;
+        a++;
+      }
+      if(smallGrades.isNotEmpty){
+        smallGradesSubject.map((e) => smallGradeAverage + e.value);
+        smallGradeAverage /= smallGradesSubject.length as double;
+        a++;
+      }
+
+      if(a == 0)return 0;
+      return (smallGradeAverage + bigGradeAverage) / a;
+
+    }
+    else{
+      double average = 0;
+      bigGradesSubject.map((e) => average += e.value*2.0);
+      smallGradesSubject.map((e) => average += e.value as double);
+
+      if(smallGradesSubject.length + bigGradesSubject.length == 0) return 0;
+      return average / (smallGradesSubject.length + bigGradesSubject.length);
+    }
+  }
+
+
 
 }
 
