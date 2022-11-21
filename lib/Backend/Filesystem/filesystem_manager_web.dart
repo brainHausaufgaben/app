38e14c5abc759a67a63e8a822c48c2552b06677f
Manuel <github@murmannmanuel.de>
2022-11-22 00:01:37 +0100

    fixeess

--- lib/Backend/Filesystem/filesystem_manager_web.dart
+++ lib/Backend/Filesystem/filesystem_manager_web.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
 
 Future<Map> parseSystemFile() async {
+  String? data;
   FileUploadInputElement uploadInput = FileUploadInputElement();
   uploadInput.click();
+
+  Completer<Map> completer = Completer();
 
   uploadInput.onChange.listen((e) async {
     final files = uploadInput.files;
@@ -16,12 +20,12 @@ Future<Map> parseSystemFile() async {
       reader.readAsText(file);
       await reader.onLoad.first;
 
-      String? data = reader.result as String?;
-      if (data != null) return jsonDecode(data);
+      data = reader.result as String?;
+      completer.complete(jsonDecode(data ?? ""));
     }
   });
 
-  return {};
+  return completer.future;
 }
 
 void saveFileToSystem(Map data) async {
